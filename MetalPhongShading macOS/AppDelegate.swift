a1b04a5825abafb316bbc771707fd7772a4fc7b1
Peter Young <young40@gmail.com>
2018-12-21 00:10:48 +0800

    init

---
+++ MetalPhongShading macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MetalPhongShading macOS
+//
+//  Created by 杨世玲 on 2018/12/21.
+//  Copyright © 2018 杨世玲. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
