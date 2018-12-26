f33af7fc8d874d81d609011c8410455471684900
Peter Young <young40@gmail.com>
2018-12-26 23:29:45 +0800

    update

---
+++ MetalPS_macOS/MenuHandler.swift
@@ -0,0 +1,15 @@
+//
+//  MenuHandler.swift
+//  MetalPS_macOS
+//
+//  Created by 杨世玲 on 2018/12/26.
+//  Copyright © 2018 杨世玲. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class MenuHander: NSObject, NSApplicationDelegate
+{
+    
+}
